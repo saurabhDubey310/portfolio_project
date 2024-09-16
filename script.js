6790904fbd3f6dc4aec55cfb18a8120e8a36c0f3
saurabh dubey <dubeysaurabh310@gmail.com>
2024-09-16 22:01:31 +0530

    add: dark mode
    - added images
    - added box shadow
    - deleted some useless images

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ function toggleMenu() {
   menu.classList.toggle("open")
   icon.classList.toggle("open")
 }
+
+const toggleSwitch = document.getElementById('switch');
+const modeText = document.querySelector('.mode');
+
+toggleSwitch.addEventListener('change', () => {
+  document.body.classList.toggle('dark-mode');
+  if (document.body.classList.contains('dark-mode')) {
+    modeText.textContent = 'Dark';
+  } else {
+    modeText.textContent = 'Light';
+  }
+});
