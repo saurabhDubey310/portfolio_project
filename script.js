44b899f0ca8e6cf9605d12730ce2d1474a43ab0c
saurabh dubey <dubeysaurabh310@gmail.com>
2024-12-03 17:16:50 +0530

    removed unused code

--- script.js
+++ script.js
@@ -5,14 +5,5 @@ function toggleMenu() {
   icon.classList.toggle("open")
 }
 
-const toggleSwitch = document.getElementById('switch');
-const modeText = document.querySelector('.mode');
 
-toggleSwitch.addEventListener('change', () => {
-  document.body.classList.toggle('dark-mode');
-  if (document.body.classList.contains('dark-mode')) {
-    modeText.textContent = 'Dark';
-  } else {
-    modeText.textContent = 'Light';
-  }
-});
+
